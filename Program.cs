f5f789689fe534874408ac648572e536bcbf27b4
viniciussouza26032005@gmail.com <e107ti@etec.sp.gov.br>
2023-03-31 20:21:53 -0300

    Projeto Concluido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿String nome, sobrenome;
+Console.Write("Digite seu nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Seu nome completo é {nome} {sobrenome}.");
+Console.WriteLine($"Seu nome de catálago é {sobrenome.ToUpper()}, {nome}.");
